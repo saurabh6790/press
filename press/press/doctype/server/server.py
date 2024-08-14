ba72c3a047785235918c78223941a40941a3f864
Saurabh <saurabh6790@gmail.com>
2024-08-14 14:37:58 +0530

    fix: move method to server class

--- press/press/doctype/server/server.py
+++ press/press/doctype/server/server.py
@@ -188,25 +188,11 @@ class BaseServer(Document, TagHelpers):
 	def validate(self):
 		self.validate_cluster()
 		self.validate_agent_password()
-		self.validate_managed_database_service()
 		if self.doctype == "Database Server" and not self.self_hosted_mariadb_server:
 			self.self_hosted_mariadb_server = self.private_ip
 
 		if not self.hostname_abbreviation:
 			self._set_hostname_abbreviation()
-
-	def validate_managed_database_service(self):
-		if not hasattr(self, "is_managed_database"):
-			return
-
-		if self.is_managed_database:
-			if not self.managed_database_service:
-				frappe.throw(_("Please select Managed Database Service"))
-
-			self.database_server = ""
-
-		else:
-			self.managed_database_service = ""
 
 	def _set_hostname_abbreviation(self):
 		self.hostname_abbreviation = get_hostname_abbreviation(self.hostname)
@@ -1190,6 +1176,23 @@ class Server(BaseServer):
 
 	GUNICORN_MEMORY = 150  # avg ram usage of 1 gunicorn worker
 	BACKGROUND_JOB_MEMORY = 3 * 80  # avg ram usage of 3 sets of bg workers
+
+	def validate(self):
+		super().validate()
+		self.validate_managed_database_service()
+
+	def validate_managed_database_service(self):
+		if not hasattr(self, "is_managed_database"):
+			return
+
+		if self.is_managed_database:
+			if not self.managed_database_service:
+				frappe.throw(_("Please select Managed Database Service"))
+
+			self.database_server = ""
+
+		else:
+			self.managed_database_service = ""
 
 	def on_update(self):
 		# If Database Server is changed for the server then change it for all the benches
